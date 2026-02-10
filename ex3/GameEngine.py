c23570e3b7a2a1ab9899e37d55d0b287327e3279
buchy16 <nominoe.buchy@gmail.com>
2026-02-10 14:05:02 +0100

    GameEngine commencé en parrallle de AggressiveStrategy

--- ex3/GameEngine.py
+++ ex3/GameEngine.py
@@ -1,16 +1,30 @@
-from ex3 import CardFactory
-from ex3 import GameStrategy
+from ex3 import CardFactory, GameStrategy
 from typing import Dict
 
 # Client side code, like the video
 
 
 class GameEngine():
-    def configure_engine(self, factory: CardFactory, strategy: GameStrategy) -> None:
-        pass
+    cards_created = 0
+
+    def __init__(self):
+        self.strategy = "No strategy used"
+        self.turn = 0
+        self.total_damage = 0
+        self.factory = None
+        self.hand1 = []
+        self.hand2 = []
+
+    def configure_engine(self, factory: CardFactory, strategy: GameStrategy, hand_size: int) -> None:
+        self.factory = factory
+        self.strategy = strategy
+        self.hand1 = self.factory().create_themed_deck(hand_size)
+        self.hand2 = self.factory().create_themed_deck(hand_size)
+
 
     def simulate_turn(self) -> Dict:
-        pass
+        self.strategy.execute_turn([card for card in self.hand1.items()],
+                                   [card for card in self.hand2.items()])
 
     def get_engine_status(self) -> Dict:
         pass
